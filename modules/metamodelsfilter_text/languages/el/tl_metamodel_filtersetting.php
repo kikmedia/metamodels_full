8d1d831267c8717eeb89d68114a931b215ea9e39
Carolina M. Koehn <office@kikmedia.de>
2013-05-14 13:27:37 +0200

    integrate languages

---
+++ modules/metamodelsfilter_text/languages/el/tl_metamodel_filtersetting.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/el/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:41+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'ξεκινάει με τον όρο αναζήτησης ';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['endswith']   = 'τελειώνει με τον όρο αναζήτησης ';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'Ακριβή αναζήτηση ';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['0']          = 'Τυπος αναζητησης';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['1']          = 'Εύρεση κομματιων κειμένου.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['text']        = 'Φιλτρο κειμενου';
---
+++ modules/metamodelsfilter_text/languages/rm/tl_metamodel_filtersetting.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:41+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'cumenza cun il term da tschertgar';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['endswith']   = 'finescha cun il term da tschertgar';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'tschertga exacta';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['0']          = 'Tip da tschertgar';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['1']          = 'Chattar parts dal text';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['text']        = 'Filter da text';
