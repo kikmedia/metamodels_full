2bb142053156ae3f0b6c9a9b8885693e2c881d03
Carolina M. Koehn <office@kikmedia.de>
2013-05-14 13:27:18 +0200

    integrate languages

---
+++ modules/metamodelsattribute_combinedvalues/languages/el/tl_metamodel_attribute.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/el/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:15:05+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['0']    = 'Πεδια';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']    = 'Παρακαλούμε επιλέξτε ένα ή περισσότερα χαρακτηριστικά για να τα συνδυάσετε. ';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['0']    = 'Τυποι';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']    = 'Χρησιμοποιήστε "% s" ή οποιοδήποτε άλλο σύμβολο κράτησης θέσης για να δημιουργήσετε ένα διαμορφωμένο κείμενο. Δείτε το <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a> για περισσότερες πληροφορίες. ';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']               = ' Χαρακτηριστικά';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['0']     = 'Force αναγέννησης';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']     = 'Ελέγξτε αυτό , αν θέλετε το άθροισμα των τιμών που πρέπει να αναγεννηθεί κάθε φορά που κάποιο από τα πεδία που εξαρτώνται αλλάζει.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['combinedvalues'] = 'Συνδυασμένες τιμές ';
---
+++ modules/metamodelsattribute_combinedvalues/languages/rm/tl_metamodel_attribute.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:15:05+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['0']    = 'Champs';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']    = 'Tscherna in u plirs attributs per cumbinar.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['0']    = 'Formats';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']    = 'Utilisescha "%s" u in auter tegnaplazza per crear in text formatà. Legia <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a> per retschaiver dapli infurmaziuns.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']               = 'Attributs';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['0']     = 'Sfurzar la re-creaziun';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']     = 'Tscherna questa chaschetta, sche ti vuls che valurs cumbinadas vegnan re-creadas mintga giada sch\'in champ dependent vegn midà.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['combinedvalues'] = 'Valurs cumbinadas';
