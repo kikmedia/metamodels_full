29c70312c476e13a8eca1c138ce3ce67def7240e
Carolina M. Koehn <office@kikmedia.de>
2013-05-15 12:10:11 +0200

    fresh-up

---
+++ modules/metamodelsattribute_tags/languages/rm/tl_metamodel_attribute.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:13:15+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Configuraziun da visualisar';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']           = 'Questa dumonda SQL chaschuna ina errur.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']      = 'Alias da l\'etichetta';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']      = 'Tscherna ina endataziun per l\'alias da l\'etichetta.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']     = 'Colonna da la tabella';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']     = 'Tscherna la colonna.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']         = 'ID da l\'etichetta';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']         = 'Tscherna ina endataziun per l\'id da l\'etichetta.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']    = 'Zavrar etichettas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']    = 'Tscherna in\'endataziun per zavrar las etichettas.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']      = 'Tabella da la banca da datas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']      = 'Tscherna la tabella da la banca da datas.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']      = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']      = 'La glista dad opziuns po vegnir limitada cun utilisar SQL.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Etichettas';
---
+++ modules/metamodelsfilter_tags/languages/rm/tl_metamodel_filtersetting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:36+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0'] = 'Be las etichettas restantas';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1'] = 'Be mussar opziuns ch\'èn anc adina assegnadas insanua suenter che quest filter è definì.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['0']     = 'Be etichettas assegnadas';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['1']     = 'Mussar be opziuns ch\'èn assegnadas insanua en il MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['tags'] = 'Selecziun multipla';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['0']        = 'OR';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['1']        = 'Colliaziun OR da las etichettas. Definì automaticamain sche l\'attribut è in element da tscherner.';
