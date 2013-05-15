ffa464e313a9acee627a2fd37aa8fb2e240977da
Carolina M. Koehn <office@kikmedia.de>
2013-05-15 12:09:44 +0200

    fresh-up

--- modules/metamodelsattribute_tags/languages/de/tl_metamodel_attribute.php
+++ modules/metamodelsattribute_tags/languages/de/tl_metamodel_attribute.php
@@ -8,22 +8,21 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:13:15+02:00
+ * last-updated: 2013-05-13T23:09:29+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Einstellungen anzeigen';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Anzeigeeinstellungen';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']           = 'Die SQL-Abfrage verursacht einen Fehler.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']      = 'Schlagwortalias';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']      = 'Bitte wählen Sie einen Eintrag für den Tag Alias aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']      = 'Alias der Mehrfachauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']      = 'Bitte wählen Sie einen Eintrag für den Alias der Mehrfachauswahl aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']     = 'Tabellenspalte';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']     = 'Bitte wählen Sie eine Spalte.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']         = 'Schlagwort ID';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']         = 'Bitte wählen Sie einen Eintrag für die Tag ID aus.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']    = 'Schlagwortsortierung';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']    = 'Bitte wählen Sie einen Eintrag für die Tag-Sortierung aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']         = 'ID der Mehrfachauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']         = 'Bitte wählen Sie einen Eintrag für die ID der Mehrfachauswahl aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']    = 'Sortierung der Mehrfachauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']    = 'Bitte wählen Sie einen Eintrag für die Sortierung der Mehrfachauswahl aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']      = 'Datenbanktabelle';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']      = 'Bitte wählen Sie eine Datenbanktabelle aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']      = 'SQL';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']      = 'Die Auswahlmöglichkeiten können per SQL eingeschränkt werden.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Schlagwort';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Mehrfachauswahl';
--- modules/metamodelsattribute_translatedtags/languages/de/tl_metamodel_attribute.php
+++ modules/metamodelsattribute_translatedtags/languages/de/tl_metamodel_attribute.php
@@ -8,12 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:13:46+02:00
+ * last-updated: 2013-05-13T23:10:20+02:00
  */
 
-
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['0']           = 'Sprachenspalte';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['1']           = 'Bitte angeben, welche Spalte den Sprach-Code in ISO 639-1 enthält';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['1']           = 'Bitte angeben, welche Spalte den Sprachcode nach ISO 639-1 enthält.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['0']           = 'Sortierspalte';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['1']           = 'Wählen Sie bitte, welche Spalte der noch nicht übersetzten Tabelle für die Sortierung verwendet werden soll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['0']             = 'Nicht übersetzte Tabelle für die Sortierung.';
